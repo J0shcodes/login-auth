cb002e080fd2675a81709a94f4060630e00e1258
Joshua Moses <mosesjoshua350@gmail.com>
2025-05-22 16:07:51 +0100

    Finished login layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,84 @@
-import Image from "next/image";
+"use client"
+
+// import Image from "next/image";
+import { useState } from "react";
+import Link from "next/link";
 import styles from "./page.module.css";
+import { FaRegEyeSlash, FaRegEye } from "react-icons/fa";
 
 export default function Home() {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+        <header className={styles.header}>
+          <h2 className={styles.h2}>Welcome Back, Evil Martians!</h2>
+          <p className={styles.p}>
+            We are excited to have you back. Log in now and access your account
+          </p>
+        </header>
+        <form className={styles.form}>
+          <div className={styles.formBody}>
+            <div className={styles.formGroup}>
+              <label htmlFor="email" className={styles.label}>
+                Email
+              </label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="evilmartians@gmail.com"
+                autoComplete="email"
+                required
+                aria-required={true}
+                aria-describedby="email-error"
+                className={styles.input}
+              />
+              <div id="email-error" className="error" role="alert"></div>
+            </div>
+            <div className={styles.formGroup} style={{ position: "relative" }}>
+              <label htmlFor="password" className={styles.label}>
+                Password
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                id="password"
+                placeholder="*******"
+                autoComplete="password"
+                required
+                aria-required={true}
+                aria-describedby="email-error"
+                className={styles.input}
+              />
+              <div id="password-error" className="password" role="alert"></div>
+              <button
+                className={styles.visibilityButton}
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <FaRegEye color="#fff" size={20} />
+                ) : (
+                  <FaRegEyeSlash color="#fff" size={20} />
+                )}
+              </button>
+            </div>
+            <div className={styles.rememberContainer}>
+              <div className={styles.remember}>
+                <input type="checkbox" id="remember" name="remember" />
+                <label htmlFor="remember">Remember me</label>
+              </div>
+              <div>
+                <Link href="/forgot_password">Forgot your password?</Link>
+              </div>
+            </div>
+          </div>
+          <button className={styles.login} type="submit">Log In</button>
+        </form>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div className={styles.blob}></div>
+      <div id="toast" className="toast" role="alert" aria-live="assertive">toast</div>
     </div>
   );
 }
